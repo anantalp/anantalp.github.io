08056995f8ef9a1b1a0d53a27a21dec31ce0cfe9
Ananta Prasannakumar <anantalp@knights.ucf.edu>
2021-05-18 00:01:42 -0400

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -75,8 +75,8 @@ exports.createSchemaCustomization = ({ actions }) => {
       github: String
       linkedin: String
       about: String
-      projects: [SectionItem]
       experience: [SectionItem]
+      projects: [SectionItem]
       skills: [SectionItem]
     }
 
